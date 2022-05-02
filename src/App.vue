6544a534908689f40f0ba8797d315280a702f915
kawuchuu <kawuchuu@users.noreply.github.com>
2022-05-02 13:43:43 +1000

    convert to vue 2

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,29 @@
 <template>
   <main>
-    <l-map style="height: 90vh" class="map-base" :zoom="zoom" :center="center">
-      <!-- <l-tile-layer :url="url" :attribution="attribution" :tile-size="tileSize" /> -->
+    <l-map class="map-base" :zoom="zoom" :center="center">
+      <l-tile-layer :url="url" :attribution="attribution" :tile-size="tileSize" :options="options" />
       <l-geo-json :geojson="geojson.aus" class="vic" />
     </l-map>
   </main>
 </template>
 
 <script>
-import "leaflet/dist/leaflet.css"
-import { LMap, LGeoJson, /* LTileLayer */ } from "@vue-leaflet/vue-leaflet";
-
 export default {
   name: 'App',
-  components: {
-    LMap,
-    LGeoJson,
-    //LTileLayer
-  },
   data() {
     return {
       geojson: {
-        vic: require('@/assets/geojson/VIC_2021.json'),
-        nsw: require('@/assets/geojson/NSW_2016.json'),
-        qld: require('@/assets/geojson/QLD_2018.json'),
-        nt: require('@/assets/geojson/NT_2017.json'),
-        wa: require('@/assets/geojson/WA_2021.json'),
-        sa: require('@/assets/geojson/SA_2018.json'),
-        tas: require('@/assets/geojson/TAS_2017.json'),
-        act: require('@/assets/geojson/ACT_2018.json'),
         aus: require('@/assets/geojson/AUS_2021.json')
       },
-      url: 'https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia2F3dWNodXUiLCJhIjoiY2t0Mjh6d2t3MGxtYjMxcGs1Nmwwd2pxdyJ9.TGiI2hgQUsNnQf8Udt9xSQ',
+      url: 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia2F3dWNodXUiLCJhIjoiY2t0Mjh6d2t3MGxtYjMxcGs1Nmwwd2pxdyJ9.TGiI2hgQUsNnQf8Udt9xSQ',
       attribution:
         'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      zoom: 5,
+      zoom: 12,
       center: [-37.8136, 144.957],
       tileSize: 512,
+      options: {
+        zoomOffset: -1,
+      }
     }
   }
 }
@@ -49,6 +36,22 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  margin: 0px;
+}
+
+main {
+  width: 100vw;
+  height: 100vh;
+}
+
+.map-base {
+  width: 100%;
+  height: 100%;
+  justify-self: center;
+  position: relative;
+  z-index: 1;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import "leaflet/dist/leaflet.css"
+import { LMap, LGeoJson, LTileLayer } from "vue2-leaflet";
 
-createApp(App).mount('#app')
+Vue.component('l-map', LMap)
+Vue.component('l-geo-json', LGeoJson)
+Vue.component('l-tile-layer', LTileLayer)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')

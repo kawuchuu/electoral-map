17df72f277524b3c3ae8650a7b32bf38acd06130
kawuchuu <kawuchuu@users.noreply.github.com>
2022-05-02 12:02:51 +1000

    add stuff woo!

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <l-map style="height: 90vh" class="map-base" :zoom="zoom" :center="center">
+      <!-- <l-tile-layer :url="url" :attribution="attribution" :tile-size="tileSize" /> -->
+      <l-geo-json :geojson="geojson.victoria" class="victoria" />
+    </l-map>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "leaflet/dist/leaflet.css"
+import { LMap, LGeoJson, /* LTileLayer */ } from "@vue-leaflet/vue-leaflet";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LMap,
+    LGeoJson,
+    //LTileLayer
+  },
+  data() {
+    return {
+      geojson: {
+        victoria: require('./assets/geojson/victoria.json')
+      },
+      url: 'https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia2F3dWNodXUiLCJhIjoiY2t0Mjh6d2t3MGxtYjMxcGs1Nmwwd2pxdyJ9.TGiI2hgQUsNnQf8Udt9xSQ',
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      zoom: 5,
+      center: [-37.8136, 144.957],
+      tileSize: 512,
+    }
   }
 }
 </script>

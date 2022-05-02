b280d3a0aac46c920c5b57f7618f76234ae0e8a0
kawuchuu <kawuchuu@users.noreply.github.com>
2022-05-02 19:49:12 +1000

    tweaked tooltip

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
     <l-map class="map-base" :zoom="zoom" :center="center">
       <l-tile-layer :url="url" :attribution="attribution" :tile-size="tileSize" :options="options" />
       <l-geo-json :geojson="geojson.aus" class="vic" :options="geoOptions" />
+      <div class="load-cover" v-if="!loaded">
+        <div class="load-spinner"/>
+      </div>
     </l-map>
   </main>
 </template>
@@ -13,7 +16,7 @@ export default {
   data() {
     return {
       geojson: {
-        aus: require('@/assets/geojson/AUS_2021.json'),
+        aus: null
       },
       url: 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia2F3dWNodXUiLCJhIjoiY2t0Mjh6d2t3MGxtYjMxcGs1Nmwwd2pxdyJ9.TGiI2hgQUsNnQf8Udt9xSQ',
       attribution:
@@ -24,7 +27,8 @@ export default {
       options: {
         zoomOffset: -1,
       },
-      polData: require('@/assets/currentelected.json')
+      polData: require('@/assets/currentelected.json'),
+      loaded: false
     }
   },
   computed: {
@@ -38,6 +42,7 @@ export default {
         let color = "#000000"
         const elName = feature.properties.Elect_div
         const electorate = this.polData[elName]
+        let coalition = false
         if (electorate) {
           switch(electorate.party) {
             case "Labor":
@@ -45,15 +50,18 @@ export default {
               break;
             case "Liberal":
               color = "#0047ab"
+              coalition = true
               break;
             case "Greens":
               color = "#10c25b"
               break;
             case "National":
               color = "#006644"
+              coalition = true
               break;
             case "Liberal National":
               color = "#1456f1"
+              coalition = true
               break;
             case "Independent":
               color = "#7c34cf"
@@ -71,8 +79,8 @@ export default {
         }
         layer.bindTooltip(
           `<h3>${feature.properties.Elect_div}</h3>
-          <p>Party: ${electorate ? electorate.party : "Unknown"}</p>
-          <p>MP: ${electorate ? electorate.mp : "Unknown"}</p>`,
+          <p style="color: ${color}">${electorate ? electorate.party : "Unknown"}${coalition ? " (Coalition)" : ""}</p>
+          <p>${electorate ? electorate.mp : "Unknown"}</p>`,
           {
             permanent: false,
             sticky: true
@@ -98,6 +106,12 @@ export default {
         })
       }
     }
+  },
+  async mounted() {
+    const reqMap = await fetch('/geojson/AUS_2021.json')
+    const mapData = await reqMap.json()
+    this.geojson.aus = mapData
+    this.loaded = true
   }
 }
 </script>
@@ -128,5 +142,48 @@ main {
   justify-self: center;
   position: relative;
   z-index: 1;
+}
+
+.load-cover {
+  width: 100%;
+  height: 100%;
+  background: rgba(0,0,0,.5);
+  position: fixed;
+  z-index: 999;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+@keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+}
+
+.load-spinner {
+    border: 4px solid white;
+    border-left: 4px solid transparent;
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+    animation: spin 1s linear infinite;
+}
+
+main .leaflet-container {
+  font-family: 'Inter';
+}
+
+main .leaflet-tooltip {
+  border-radius: 10px;
+  padding: 10px 12px;
+}
+
+main .leaflet-tooltip h3 {
+  margin: 0px 0px 5px;
+  font-family: 2.25em;
+}
+
+main .leaflet-tooltip p {
+  margin: 2px 0px;
 }
 </style>

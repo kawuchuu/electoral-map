d1e52dd8a27b220b6d8aaa650c640a8ed71b68b8
kawuchuu <kawuchuu@users.noreply.github.com>
2022-05-02 12:29:47 +1000

    add data weee

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <main>
     <l-map style="height: 90vh" class="map-base" :zoom="zoom" :center="center">
       <!-- <l-tile-layer :url="url" :attribution="attribution" :tile-size="tileSize" /> -->
-      <l-geo-json :geojson="geojson.victoria" class="victoria" />
+      <l-geo-json :geojson="geojson.aus" class="vic" />
     </l-map>
   </main>
 </template>
@@ -21,7 +21,15 @@ export default {
   data() {
     return {
       geojson: {
-        victoria: require('./assets/geojson/victoria.json')
+        vic: require('@/assets/geojson/VIC_2021.json'),
+        nsw: require('@/assets/geojson/NSW_2016.json'),
+        qld: require('@/assets/geojson/QLD_2018.json'),
+        nt: require('@/assets/geojson/NT_2017.json'),
+        wa: require('@/assets/geojson/WA_2021.json'),
+        sa: require('@/assets/geojson/SA_2018.json'),
+        tas: require('@/assets/geojson/TAS_2017.json'),
+        act: require('@/assets/geojson/ACT_2018.json'),
+        aus: require('@/assets/geojson/AUS_2021.json')
       },
       url: 'https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia2F3dWNodXUiLCJhIjoiY2t0Mjh6d2t3MGxtYjMxcGs1Nmwwd2pxdyJ9.TGiI2hgQUsNnQf8Udt9xSQ',
       attribution:
